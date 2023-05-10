fcc0fa893c8e0bc22cff657eb1ae4c0ee952c557
Bharathi978 <sharathibharathi@gmail.com>
2023-05-10 13:29:25 +0530

    verfy

---
+++ src/org/phone/ExternalStroage.java
@@ -0,0 +1,27 @@
+package org.phone;
+
+public class ExternalStroage {
+	private void size() {
+System.out.println("size");
+
+	}
+	public static void main(String[] args) {
+		ExternalStroage m =new ExternalStroage();
+		m.size();
+		InternalStorage l =new InternalStorage();
+		l.processorName();
+		l.ramSize();
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+
+}
